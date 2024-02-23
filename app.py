e3da257a3e93611428a995fd02dbcb37940e3456
Phanos Hadjikyriakou <hadjikyriakou.phanos@gmail.com>
2024-02-23 09:48:13 +0000

    viktor is preeeeeeetty cool

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
-# Welcome to VIKTOR!
-#
-# Get started by running the following command in the terminal below:
-#   viktor-cli create-app --demo
+from viktor import ViktorController
+from viktor.parametrization import ViktorParametrization
+from viktor.geometry import SquareBeam 
+from viktor.views import GeometryView, GeometryResult
+
+
+
+class Parametrization(ViktorParametrization):
+    pass
+
+
+class Controller(ViktorController):
+    label = 'My Entity Type'
+    parametrization = Parametrization
+
+
+class Controller(ViktorController):
+    label = "your entity type"
+    parametrization = Parametrization
+
+    @GeometryView("3D Geometry", duration_guess=1)
+    def get_geometry(self, params, **kwargs):
+        block = SquareBeam(
+            length_x=1,
+            length_y=1,
+            length_z=1
+        )
+        return GeometryResult(block)
